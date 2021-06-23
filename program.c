5d4733d48c38e5c07894c4f23983f6366ac6f165
Bartosz Wroński <bartek-wronski7@wp.pl>
2021-06-23 11:15:56 +0200

    Ćwiczenie 6 włącznie.

--- program.c
+++ program.c
@@ -55,6 +55,7 @@ void gora_i_dol(int n)
 *
 */
 
+/*
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -89,3 +90,188 @@ unsigned long Fibonaci(int n)
         return 1;
     } 
 }
+*/
+
+
+
+
+/*
+*   Ćwiczenie pierwsze na koniec rozdziału
+*                   #
+*                 # #
+*                #  #
+*                   #
+*                   #
+*                 # # # 
+*/                  
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+double min(double x, double y);
+
+int main(void)
+{
+    double liczba =0;
+    double liczba_2 = 0;
+
+    printf("Podaj liczby do porównania.\n");
+
+    while(((scanf("%le",&liczba_2))==1)&&((scanf("%le",&liczba))==1))
+    {
+        liczba=min(liczba,liczba_2);
+        printf("Większą liczbą jest: %4.2f.\n\n",liczba);
+        printf("Podaj liczby do porównania.\n");
+    }
+
+
+    return 0;
+}
+
+
+
+double min(double x, double y)
+{
+    if(x>=y)
+    {
+        return x;
+    }
+    else
+    {
+        return y;
+    }
+}
+*/
+
+
+/*
+*   Ćwiczenie piąte na koniec rozdziału
+*               #######
+*               #  
+*               #
+*               ###### 
+*                     #
+*                     #
+*               ###### 
+*/ 
+
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(double *x, double *y);  // * oznaczaja, że odwołujemy się do wartości, na które wskazują wskaźniki
+
+int main(void)
+{
+    double liczba =0;
+    double liczba_2 = 0;
+
+    printf("Podaj liczby do zamiany.\n");
+
+    while(((scanf("%le",&liczba_2))==1)&&((scanf("%le",&liczba))==1))
+    {
+        printf("Liczby przed zamianą: x = %4.2f, y = %4.2f.\n",liczba,liczba_2);
+        swap(&liczba,&liczba_2);
+        printf("Liczby po zamianie: x = %4.2f, y = %4.2f.\n\n",liczba,liczba_2);
+        printf("Podaj liczby do zamiany.\n");
+    }
+
+
+    return 0;
+}
+
+
+
+void swap (double * x, double * y)   //Argumentami sa adresy pamieci do zmiennych
+{
+   double  tmp;                         // Tworzymy zmienna pomocniczą
+
+   tmp=*y;                              //do wartości tmp przypisujemy wartosc na którą wskazuje y (* jest to znak dereferencji)
+   *y=*x;                               //do wartości wskazywanej przez y przypisujemy wartosc wskazywaną przez x
+   *x=tmp;                              //do wartości wskazywanej przez x przypisujemy wartość tmp
+}
+*/
+
+
+
+
+/*
+*   Ćwiczenie szóste na koniec rozdziału
+*                #####
+*               #     #
+*               #
+*               ###### 
+*               #     #
+*               #     #
+*                ##### 
+*/ 
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void zamien(double * x, double * y, double * z);  // * oznaczaja, że odwołujemy się do wartości, na które wskazują wskaźniki
+
+int main(void)
+{
+    double liczba =0;
+    double liczba_2 = 0;
+    double liczba_3 = 0;
+
+
+    printf("Podaj liczby do zamiany.\n");
+
+    while(((scanf("%le",&liczba))==1)&&((scanf("%le",&liczba_2))==1)&&((scanf("%le",&liczba_3))==1))
+    {
+        printf("Liczby przed zamianą: x = %4.2f, y = %4.2f, z = %4.2f.\n",liczba,liczba_2,liczba_3);
+        zamien(&liczba,&liczba_2,&liczba_3);
+        printf("Liczby po zamianie: x = %4.2f, y = %4.2f, z = %4.2f.\n\n",liczba,liczba_2,liczba_3);
+        printf("Podaj liczby do zamiany.\n");
+    }
+
+
+    return 0;
+}
+
+
+
+
+void zamien(double * x, double * y, double * z)  // * oznaczaja, że odwołujemy się do wartości, na które wskazują wskaźniki
+{
+   double  tmp;                         // Tworzymy zmienna pomocniczą
+
+    if(*x-*y>0)
+    {
+        tmp=*y;
+        *y=*x;
+        *x=tmp;
+    }
+
+    if(*y-*z>0)
+    {
+        tmp=*z;                     //do wartości tmp przypisujemy wartosc na którą wskazuje y (* jest to znak dereferencji)
+        *z=*x;                      //do wartości wskazywanej przez y przypisujemy wartosc wskazywaną przez x
+        *x=tmp;                     //do wartości wskazywanej przez x przypisujemy wartość tmp
+    }
+
+
+        if(*z-*y<0)
+    {
+        tmp=*z;                     //do wartości tmp przypisujemy wartosc na którą wskazuje y (* jest to znak dereferencji)
+        *z=*y;                      //do wartości wskazywanej przez y przypisujemy wartosc wskazywaną przez x
+        *y=tmp;                     //do wartości wskazywanej przez x przypisujemy wartość tmp
+    } 
+
+     if(*z-*x<0)
+    {
+        tmp=*z;
+        *z=*x;
+        *x=tmp;
+    }                         
+                                                              
+}
+
+
